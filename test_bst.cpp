8a259936dc75f6e929654a41d924f16034088390
RedNelson <137230162+RedHeadFred923@users.noreply.github.com>
2025-11-02 19:39:30 -0500

    Update test_bst.cpp

---
+++ test_bst.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "usecase.cpp"
+
+using namespace std;
+
+
+
+void test_empty(){
+
+}
+
+void test_insert(){
+
+}
+
+void test_get(){
+
+}
+
+void test_remove(){
+
+}
+
+void test_max_data(){
+
+}
+
+void test_max_key(){
+
+}
+
+void test_min_data(){
+
+}
+
+void test_min_key(){
+
+}
+
+void test_successor(){
+
+}
+
+void test_in_order(){
+
+}
+
+void test_trim(){
+
+}
+
+void test_usecase(){
+
+}
+
+
+int main(){
+
+    test_empty();
+    test_insert();
+    test_get();
+    test_remove();
+    test_max_data();
+    test_max_key();
+    test_min_data();
+    test_min_key();
+    test_successor();
+    test_in_order();
+    test_trim();
+    test_usecase();
+
+    return 0;
+}
