326174d1455acaad791c3f02b3481701c2c2c868
RedNelson <137230162+RedHeadFred923@users.noreply.github.com>
2025-11-03 00:07:46 -0500

    Update test_bst.cpp

--- test_bst.cpp
+++ test_bst.cpp
@@ -6,14 +6,21 @@ using namespace std;
 
 
 void test_empty(){
-    BST<int, int> tester;
-    if(!tester.empty()){
-        cout << "Empty failed. Expected empty tree but got: " << tester.to_string() << endl;
+    try
+    {
+        BST<int, int> tester;
+        if(!tester.empty()){
+            cout << "Empty failed. Expected empty tree but got: " << tester.to_string() << endl;
+        }
+        tester.insert(10,12);
+        tester.insert(-10,-12);
+        if(tester.empty()){
+            cout << "Empty failed. Expected tree with 2 elements (10,12 & -10,-12) but got: " << tester.to_string() << endl;
+        }
     }
-    tester.insert(10,12);
-    tester.insert(-10,-12);
-    if(tester.empty()){
-        cout << "Empty failed. Expected tree with 2 elements (10,12 & -10,-12) but got: " << tester.to_string() << endl;
+    catch (exception &e)
+    {
+        cerr << "Error getting keys in_order from bst : " << e.what() << endl;
     }
 }
 
@@ -30,66 +37,93 @@ void test_remove(){
 }
 
 void test_max_data(){
-    BST<int, int> tester;
-    if(tester.max_data() != int{}){
-        cout << "Max data on an empty tree failed. Expected return type int{} but got: " << tester.max_data() << endl;
+    try
+    {
+        BST<int, int> tester;
+        if(tester.max_data() != int{}){
+            cout << "Max data on an empty tree failed. Expected return type int{} but got: " << tester.max_data() << endl;
+        }
+        
+        //test zigzag tree
+        tester.insert(10,12);
+        tester.insert(3,17);
+        tester.insert(7,13);
+        if(tester.max_data() != 3){
+            cout << "Max data on zigzag tree failed. Expected 3 but got: " << tester.max_data() << endl;
+            cout << "From the tree: " << tester.to_string() << endl;
+        }
     }
-    
-    //test zigzag tree
-    tester.insert(10,12);
-    tester.insert(3,17);
-    tester.insert(7,13);
-    if(tester.max_data() != 3){
-        cout << "Max data on zigzag tree failed. Expected 3 but got: " << tester.max_data() << endl;
-        cout << "From the tree: " << tester.to_string() << endl;
+    catch (exception &e)
+    {
+        cerr << "Error getting keys in_order from bst : " << e.what() << endl;
     }
 }
 
 void test_max_key(){
-    BST<int, int> tester;
-    if(tester.max_key() != int{}){
-        cout << "Max key on an empty tree failed. Expected return type int{} but got: " << tester.max_key() << endl;
+    try
+    {
+        BST<int, int> tester;
+        if(tester.max_key() != int{}){
+            cout << "Max key on an empty tree failed. Expected return type int{} but got: " << tester.max_key() << endl;
+        }
+        
+        //test zigzag tree
+        tester.insert(10,12);
+        tester.insert(3,17);
+        tester.insert(7,13);
+        if(tester.max_key() != 17){
+            cout << "Max key on zigzag tree failed. Expected 17 but got: " << tester.max_key() << endl;
+            cout << "From the tree: " << tester.to_string() << endl;
+        }
     }
-    
-    //test zigzag tree
-    tester.insert(10,12);
-    tester.insert(3,17);
-    tester.insert(7,13);
-    if(tester.max_key() != 17){
-        cout << "Max key on zigzag tree failed. Expected 17 but got: " << tester.max_key() << endl;
-        cout << "From the tree: " << tester.to_string() << endl;
+    catch (exception &e)
+    {
+        cerr << "Error getting keys in_order from bst : " << e.what() << endl;
     }
 }
 
 void test_min_data(){
-    BST<int, int> tester;
-    if(tester.min_data() != int{}){
-        cout << "Min data on an empty tree failed. Expected return type int{} but got: " << tester.min_data() << endl;
+    try
+    {
+        BST<int, int> tester;
+        if(tester.min_data() != int{}){
+            cout << "Min data on an empty tree failed. Expected return type int{} but got: " << tester.min_data() << endl;
+        }
+        
+        //test zigzag tree
+        tester.insert(-10,-12);
+        tester.insert(-3,-17);
+        tester.insert(-7,-13);
+        if(tester.max_data() != -3){
+            cout << "Min data on zigzag tree failed. Expected -3 but got: " << tester.min_data() << endl;
+            cout << "From the tree: " << tester.to_string() << endl;
+        }
     }
-    
-    //test zigzag tree
-    tester.insert(-10,-12);
-    tester.insert(-3,-17);
-    tester.insert(-7,-13);
-    if(tester.max_data() != -3){
-        cout << "Min data on zigzag tree failed. Expected -3 but got: " << tester.min_data() << endl;
-        cout << "From the tree: " << tester.to_string() << endl;
+    catch (exception &e)
+    {
+        cerr << "Error getting keys in_order from bst : " << e.what() << endl;
     }
 }
 
 void test_min_key(){
-    BST<int, int> tester;
-    if(tester.min_key() != int{}){
-        cout << "Min key on an empty tree failed. Expected return type int{} but got: " << tester.min_key() << endl;
+    try
+    {
+        BST<int, int> tester;
+        if(tester.min_key() != int{}){
+            cout << "Min key on an empty tree failed. Expected return type int{} but got: " << tester.min_key() << endl;
+        }
+        
+        //test zigzag tree
+        tester.insert(-10,-12);
+        tester.insert(-3,-17);
+        tester.insert(-7,-13);
+        if(tester.min_key() != -17){
+            cout << "Min key on zigzag tree failed. Expected -17 but got: " << tester.min_key() << endl;
+            cout << "From the tree: " << tester.to_string() << endl;
+        }
     }
-    
-    //test zigzag tree
-    tester.insert(-10,-12);
-    tester.insert(-3,-17);
-    tester.insert(-7,-13);
-    if(tester.min_key() != -17){
-        cout << "Min key on zigzag tree failed. Expected -17 but got: " << tester.min_key() << endl;
-        cout << "From the tree: " << tester.to_string() << endl;
+    catch (exception &e) {
+        cerr << "Error in determining successor in bst : " << e.what() << endl;
     }
 }
 
@@ -166,20 +200,27 @@ void test_trim() {
 }
 
 void test_usecase(){
-    BST<string, string>* binHex = create_bst("binhex.txt");
-    string bin = "";
-    if(convert(binHex, bin) != ""){
-        cout << "Usecase failed. Expected \"\" but got: " << convert(binHex, bin) << endl;
-    }
+    try
+    {
+        BST<string, string>* binHex = create_bst("binhex.txt");
+        string bin = "";
+        if(convert(binHex, bin) != ""){
+            cout << "Usecase failed. Expected \"\" but got: " << convert(binHex, bin) << endl;
+        }
 
-    bin = "1";
-    if(convert(binHex, bin) != "1"){
-        cout << "Usecase failed. Expected \"1\" but got: " << convert(binHex, bin) << endl;
-    }
+        bin = "1";
+        if(convert(binHex, bin) != "1"){
+            cout << "Usecase failed. Expected \"1\" but got: " << convert(binHex, bin) << endl;
+        }
 
-    bin = "10011011110001001010010101011111000010100";
-    if(convert(binHex, bin) != "1"){
-        cout << "Usecase failed. Expected \"137894ABE14\" but got: " << convert(binHex, bin) << endl;
+        bin = "10011011110001001010010101011111000010100";
+        if(convert(binHex, bin) != "1"){
+            cout << "Usecase failed. Expected \"137894ABE14\" but got: " << convert(binHex, bin) << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error getting keys in_order from bst : " << e.what() << endl;
     }
 }
 
